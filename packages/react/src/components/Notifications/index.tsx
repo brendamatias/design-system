875a6d3785a366860a2f94ab060cc5763de740c8
Brenda Matias <brendamatias.sobral@gmail.com>
2023-04-24 22:54:04 -0300

    refactor: notifications component

--- packages/react/src/components/Notifications/index.tsx
+++ packages/react/src/components/Notifications/index.tsx
@@ -1,9 +1,20 @@
-import { ComponentProps } from 'react'
-import { NotificationsButton, NotificationsContainer } from './styles'
-import { useToggle } from '../../utils'
+import {
+  Arrow,
+  Container,
+  Header,
+  Icon,
+  Item,
+  ItemInfo,
+  List,
+  NotificationsButton,
+} from './styles'
+import { formatTimeAgo } from '../../utils'
 import { NotificationIcon } from '../../assets/icons'
 import classNames from 'classnames'
-import { Dropdown } from './Dropdown'
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
+import { Text } from '../Text'
+import { Button } from '../Button'
+import { MdDns } from 'react-icons/md'
 
 export type Notification = {
   id: string
@@ -12,30 +23,75 @@ export type Notification = {
   unread: boolean
   createdAt: string
 }
-export interface NotificationsProps
-  extends ComponentProps<typeof NotificationsContainer> {
+export interface NotificationsProps {
   notifications: Notification[]
 }
 
 export function Notifications({ notifications, ...props }: NotificationsProps) {
-  const [isDropdownVisible, toggleDropdownVisibility] = useToggle(false)
-
   return (
-    <NotificationsContainer {...props}>
-      <NotificationsButton
-        onClick={toggleDropdownVisibility}
-        className={classNames({
-          active: isDropdownVisible,
-          'has-unread': notifications.some(
-            (notification) => notification.unread,
-          ),
-        })}
-      >
-        <NotificationIcon size={22} />
-      </NotificationsButton>
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger asChild>
+        <NotificationsButton
+          className={classNames({
+            'has-unread': notifications.some(
+              (notification) => notification.unread,
+            ),
+          })}
+        >
+          <NotificationIcon size={22} />
+        </NotificationsButton>
+      </DropdownMenu.Trigger>
 
-      <Dropdown notifications={notifications} visible={isDropdownVisible} />
-    </NotificationsContainer>
+      <DropdownMenu.Portal>
+        <Container sideOffset={5}>
+          <Header>
+            <Text as="strong">Notificações</Text>
+            <Button variant="tertiary" size="sm">
+              Marcar todas como lidas
+            </Button>
+          </Header>
+
+          <List>
+            {notifications.length > 0 ? (
+              <>
+                {notifications.map(
+                  ({ id, content, unread, demand, createdAt }) => (
+                    <Item key={id} className={classNames({ unread })}>
+                      <ItemInfo>
+                        <Icon>
+                          <MdDns />
+                        </Icon>
+                        <div>
+                          <Text
+                            size="sm"
+                            dangerouslySetInnerHTML={{ __html: content }}
+                          />
+                          <Text as="span" size="xs">
+                            {demand}
+                          </Text>
+                        </div>
+                      </ItemInfo>
+
+                      <Text as="span" size="xxs">
+                        {formatTimeAgo(new Date(createdAt))}
+                      </Text>
+                    </Item>
+                  ),
+                )}
+              </>
+            ) : (
+              <Text size="sm">Nada por aqui por enquanto!</Text>
+            )}
+          </List>
+
+          <Button variant="tertiary" size="sm">
+            Ver todas
+          </Button>
+
+          <Arrow />
+        </Container>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
   )
 }
 
--- packages/react/src/components/Notifications/styles.ts
+++ packages/react/src/components/Notifications/styles.ts
@@ -1,9 +1,13 @@
 import { styled } from '../../styles'
-
-export const NotificationsContainer = styled('div', {
-  display: 'inline-block',
-  position: 'relative',
-})
+import {
+  slideDownAndFade,
+  slideLeftAndFade,
+  slideRightAndFade,
+  slideUpAndFade,
+} from '../../styles/animations'
+import { Button } from '../Button'
+import { Text } from '../Text'
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 
 export const NotificationsButton = styled('button', {
   display: 'flex',
@@ -34,7 +38,154 @@ export const NotificationsButton = styled('button', {
     color: '$gray600',
   },
 
-  '&:hover, &.active': {
+  '&:hover, &:focus, &[data-state="open"]': {
     backgroundColor: '$gray100',
+    border: '0px !important',
   },
+})
+
+export const Container = styled(DropdownMenu.Content, {
+  boxShadow: 'rgba(0, 0, 0, 0.3) 3px 2px 20px',
+  borderRadius: '$sm',
+  background: '$white',
+  width: 450,
+  willChange: 'transform, opacity',
+  animationDuration: '400ms',
+
+  [`> ${Button}`]: {
+    width: '100%',
+    padding: '$4 0',
+    borderTop: '1px solid $gray100',
+  },
+
+  '> div': {
+    maxHeight: 350,
+    overflowY: 'auto',
+
+    '&::-webkit-scrollbar': {
+      height: '$2',
+      width: '$2',
+    },
+
+    '&::-webkit-scrollbar-track': {
+      background: '$gray100',
+    },
+
+    '&::-webkit-scrollbar-thumb': {
+      background: '$vibbra300',
+      borderRadius: '$sm',
+
+      '&:hover': {
+        background: '$vibbra500',
+      },
+    },
+  },
+
+  '&[data-side="top"]': {
+    animationName: `${slideDownAndFade}`,
+  },
+
+  '&[data-side="right"]': {
+    animationName: `${slideLeftAndFade}`,
+  },
+
+  '&[data-side="bottom"]': {
+    animationName: `${slideUpAndFade}`,
+    margin: '0 $5 0 $5',
+  },
+
+  '&[data-side="left"]': {
+    animationName: `${slideRightAndFade}`,
+  },
+})
+
+export const Header = styled('header', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  paddingBottom: '$6',
+  padding: '$4',
+})
+
+export const List = styled('div', {
+  maxHeight: 350,
+  overflowY: 'auto',
+
+  '&::-webkit-scrollbar': {
+    height: '$2',
+    width: '$2',
+  },
+
+  '&::-webkit-scrollbar-track': {
+    background: '$gray100',
+  },
+
+  '&::-webkit-scrollbar-thumb': {
+    background: '$vibbra300',
+    borderRadius: '$sm',
+
+    '&:hover': {
+      background: '$vibbra500',
+    },
+  },
+
+  [`> ${Text}`]: {
+    borderTop: '1px solid $gray100',
+    textAlign: 'center',
+    padding: '$10 0',
+  },
+})
+
+export const Item = styled('div', {
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'flex-start',
+  justifyContent: 'space-between',
+  gap: '$5',
+  borderTop: '1px solid $gray100',
+  padding: '$4',
+
+  '&.unread': {
+    backgroundColor: '#F3F6FF',
+  },
+
+  span: {
+    color: '$gray200',
+  },
+
+  [`> ${Text}`]: {
+    whiteSpace: 'nowrap',
+  },
+})
+
+export const ItemInfo = styled('div', {
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  gap: '$3',
+
+  '> div': {
+    [`> ${Text}:first-child`]: {
+      display: '-webkit-box',
+      overflow: 'hidden',
+      lineClamp: 2,
+      '-webkit-line-clamp': 2,
+      '-webkit-box-orient': 'vertical',
+    },
+  },
+})
+
+export const Icon = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  borderRadius: '$full',
+  width: '$7',
+  height: '$7',
+  background: '$vibbra300',
+  color: '$white',
+})
+
+export const Arrow = styled(DropdownMenu.DropdownMenuArrow, {
+  fill: '$white',
 })
